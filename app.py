ca586770a250758b1df4ebec5861e26dfd295ae1
Sai Darshan <saidar9632@gmail.com>
2024-10-20 13:44:21 +0530

    upload

--- app.py
+++ app.py
@@ -251,16 +251,7 @@ def generate_report():
         ("57044254900", "Dr CECIL DONALD A", "https://scholar.google.co.in/citations?user=_bbxYHsAAAAJ&hl=en&authuser=1"),
         ("57055221400", "Dr CHANDRA J", "https://scholar.google.com/citations?user=bn6WQUoAAAAJ"),
         ("59256484700", "Dr CYNTHIA T", "https://scholar.google.com/citations?hl=en&user=ThELNO0AAAAJ"),
-        ("57162822500", "Dr DEEPA V JOSE", "https://scholar.google.co.in/citations?user=ryhyx4IAAAAJ&hl=en"),
-        ("57205027677", "FABIOLA HAZEL POHRMEN", "https://scholar.google.com/citations?user=prcv4fAAAAAJ&hl=en&oi=ao"),
-        ("57192668092", "Gobi Ramasamy", "https://scholar.google.com/citations?user=eu_o414AAAAJ&hl=en"),
-        ("55811681700", "Helen k Joy", "https://scholar.google.com/citations?hl=en&user=uZXv4XIAAAAJ"),
-        ("57205128308", "Hubert Shanthan", "https://scholar.google.com/citations?user=Cf2I4OoAAAAJ&hl=en"),
-        ("57789387000", "R Kavitha", "https://scholar.google.com/citations?hl=en&user=Li0r8uMAAAAJ"),
-        ("35069671200", "V.B.KIRUBANAND", "https://scholar.google.co.in/citations?user=FlLJ1SYAAAAJ&hl=en"),
-        ("35332112400", "Dr. MANJUNATHA HIREMATH", "https://scholar.google.co.in/citations?user=AIdTGncAAAAJ&hl=en"),
-        ("58529182900", "Dr Mohana Priya T", "https://scholar.google.com/citations?user=XPjU9AIAAAAJ&hl=en&authuser=1"),
-        ("57193578932", "Nismon Rio Robert", "https://scholar.google.co.in/citations?hl=en&user=Z34wmvMAAAAJ")
+        ("57162822500", "Dr DEEPA V JOSE", "https://scholar.google.co.in/citations?user=ryhyx4IAAAAJ&hl=en")
 
     ]
 
